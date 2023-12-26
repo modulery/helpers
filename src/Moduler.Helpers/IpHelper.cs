e0c83cef112a60d99b2016be0740b95a356f0095
mesuttasci <mesuttascic@gmail.com>
2023-12-27 01:59:13 +0300

    MyWebProxy

--- src/Moduler.Helpers/IpHelper.cs
+++ src/Moduler.Helpers/IpHelper.cs
@@ -1,4 +1,7 @@
-﻿using System.Net;
+﻿using RestSharp;
+using System;
+using System.Net;
+using System.Net.Http;
 using System.Net.Sockets;
 
 namespace Moduler.Helpers
@@ -17,6 +20,54 @@ namespace Moduler.Helpers
             }
 
             return string.Empty;
+        }
+        private static string? _HostPublicIp { get; set; }
+        public static string? GetHostPublicIp
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_HostPublicIp))
+                {
+                    string url = "http://ifconfig.me/ip";
+
+                    using (HttpClient client = new HttpClient())
+                    {
+                        try
+                        {
+                            _HostPublicIp = client.GetStringAsync(url).Result;
+                            Console.WriteLine(_HostPublicIp);
+                        }
+                        catch (HttpRequestException ex)
+                        {
+                            Console.WriteLine($"Hata oluştu: {ex.Message}");
+                        }
+                    }
+                }
+                return _HostPublicIp;
+            }
+        }
+        private static string? _ProxyIp { get; set; }
+        public static string? GetProxyIp
+        {
+            get
+            {
+                if (MyWebProxy.UseWebProxy && string.IsNullOrEmpty(_ProxyIp))
+                {
+                    string url = "http://ifconfig.me/ip";
+                    var client = MyWebProxy.GetClient(url.ToString());
+                    try
+                    {
+                        var request = new RestRequest(url.ToString(), method: Method.Get);
+                        _ProxyIp = client.Get(request).Content;
+                        Console.WriteLine(_ProxyIp);
+                    }
+                    catch (HttpRequestException ex)
+                    {
+                        Console.WriteLine($"Hata oluştu: {ex.Message}");
+                    }
+                }
+                return _ProxyIp;
+            }
         }
     }
 }
---
+++ src/Moduler.Helpers/MyWebProxy.cs
@@ -0,0 +1,37 @@
+﻿using RestSharp;
+using System.Net;
+
+namespace Moduler.Helpers
+{
+    public class MyWebProxyConfig
+    {
+        public static MyWebProxyConfig GetConfig { get; set; }
+        public string WebProxyIp { get; set; }
+        public string WebProxyUserName { get; set; }
+        public string WebProxyPassword { get; set; }
+    }
+    public static class MyWebProxy
+    {
+        public static bool UseWebProxy;
+        public static WebProxy GetWebProxy => new WebProxy(MyWebProxyConfig.GetConfig.WebProxyIp)
+        {
+            Credentials = new NetworkCredential(MyWebProxyConfig.GetConfig.WebProxyUserName, MyWebProxyConfig.GetConfig.WebProxyPassword)
+        };
+        public static RestClient GetClient(string url)
+        {
+            var client = new RestClient(url);
+            if (UseWebProxy)
+            {
+                WebProxy proxy = GetWebProxy;
+                var clientOptions = new RestClientOptions { Proxy = proxy };
+                client = new RestClient(clientOptions);
+            }
+            else
+            {
+                var clientOptions = new RestClientOptions();
+                client = new RestClient(clientOptions);
+            }
+            return client;
+        }
+    }
+}
